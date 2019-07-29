60007852a79a67418e800ae8d126b7655b2b4c94
Daniel Bergey <bergey@teallabs.org>
2019-07-29 21:36:42 +0000

    update Nix for monadlist

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { mkDerivation, array, base, bytestring, c2hs, clock, CLUtil
 , colour, containers, deepseq, diagrams-lib, directory, file-embed
 , filepath, FontyFruity, half, happy, hashable, IfElse, JuicyPixels
-, lens, linear, loop, mmorph, monad-memo, MonadRandom, mtl, OpenCL
-, OpenGL, OpenGLRaw, parallel, parallel-io, random, sdl2, silently
-, stdenv, text, time, transformers, vector
+, lens, linear, loop, mmorph, monad-memo, monadlist, MonadRandom
+, mtl, OpenCL, OpenGL, OpenGLRaw, parallel, parallel-io, random
+, sdl2, silently, stdenv, text, time, transformers, vector
 }:
 mkDerivation {
   pname = "gudni";
@@ -14,9 +14,9 @@ mkDerivation {
   libraryHaskellDepends = [
     array base bytestring clock CLUtil colour containers deepseq
     diagrams-lib directory file-embed filepath FontyFruity half
-    hashable JuicyPixels lens linear loop mmorph monad-memo MonadRandom
-    mtl OpenCL OpenGL OpenGLRaw parallel parallel-io random sdl2 text
-    time transformers vector
+    hashable JuicyPixels lens linear loop mmorph monad-memo monadlist
+    MonadRandom mtl OpenCL OpenGL OpenGLRaw parallel parallel-io random
+    sdl2 text time transformers vector
   ];
   libraryToolDepends = [ c2hs happy ];
   executableHaskellDepends = [
